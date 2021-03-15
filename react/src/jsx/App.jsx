a1647be8c4c38f0738d8580cb6df79ddf613cb68
SuperTikuwa <tikuwamk2@gmail.com>
2021-03-15 15:02:42 +0900

    reactを初期化しました

---
+++ react/src/jsx/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { connect } from "redux"
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return <div></div>
+	}
+}
+
+export default connect()(App)
---
+++ react/src/jsx/index.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+
+let root = document.querySelector("#root")
+ReactDOM.render(<Provider></Provider>, root)
