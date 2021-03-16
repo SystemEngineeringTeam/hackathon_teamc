112339a47a5ba41e9f85f61577a610d5cc984156
nissy <nissy@nissynoMacBook-Pro.local>
2021-03-16 10:10:42 +0900

    add databasecode(kari)

---
+++ java/database/src/usersql/SelectUserSql.java
@@ -0,0 +1,40 @@
+package usersql;
+
+import java.sql.*;
+
+public class SelectUserSql {
+    public UsersData selectusersql(String eml){
+        Connection conn = null;
+        Statement stmt = null;
+        UsersData rtn = null;
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost/app_db", "hoge", "hogehoge");
+            String dt = "SELECT *" +
+                            "FROM users" +
+                            "WHERE mailaddress = ?;";
+            PreparedStatement sql = conn.prepareStatement(dt);
+            sql.setString(1,eml);
+            ResultSet hrs = sql.executeQuery();
+            rtn.AddData(hrs.getString("name"),hrs.getString("mailaddress"),hrs.getString("pass"));
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return rtn;
+    }
+}
