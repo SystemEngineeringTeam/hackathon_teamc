4c4683688a3561b40709af2405226dd4ba44b949
tikuwa <tikuwamk2@gmail.com>
2021-03-17 09:18:36 +0900

    [add] 登録フォームのコンポーネント完成

--- react/src/jsx/SignUp/UserForm.jsx
+++ react/src/jsx/SignUp/UserForm.jsx
@@ -17,21 +17,32 @@ class UserForm extends React.Component {
 		e.preventDefault()
 		let email = document.querySelector("#email").value
 		let password = document.querySelector("#password").value
+		let passwordChecker = document.querySelector("#password-check").value
 
 		let re = new RegExp("^[a-zA-Z0-9.?/-]{8,24}$")
 		if (re.test(password)) {
 			this.setState({
 				html: (
-					<Typography color="primary">
+					<Typography color="primary" align="center">
 						ユーザー登録が完了しました． <br />
 						メールを確認してください．
 					</Typography>
 				),
 			})
+
+			if (password != passwordChecker) {
+				this.setState({
+					html: (
+						<Typography color="error" align="center">
+							パスワードが一致しません
+						</Typography>
+					),
+				})
+			}
 		} else {
 			this.setState({
 				html: (
-					<Typography color="error">
+					<Typography color="error" align="center">
 						パスワードは8文字以上入力してください
 					</Typography>
 				),
@@ -45,47 +56,80 @@ class UserForm extends React.Component {
 		return (
 			<div>
 				<Grid container alignItems="center" justify="center">
-					<Grid item xs={12}>
+					<Grid
+						container
+						alignItems="center"
+						justify="center"
+						alignContent="center"
+						item
+						xs={12}
+					>
 						<Paper variant="outlined" elevation={3}>
-							<form onSubmit={this.doSubmit}>
-								<Grid container item xs={10}>
-									<TextField
-										type="email"
-										placeholder="email"
-										variant="outlined"
-										id="email"
-										required
-									></TextField>
-								</Grid>
-								<Grid container item xs={10}>
-									<TextField
-										type="password"
-										placeholder="password"
-										variant="outlined"
-										id="password"
-										required
-										minLength="8"
-									></TextField>
-								</Grid>
-								<Grid container item xs={10}>
-									<TextField
-										type="password"
-										placeholder="password"
-										variant="outlined"
-										id="password"
-										required
-										minLength="8"
-									></TextField>
-								</Grid>
-								<Grid container item xs={10}>
-									<Button type="submit" variant="contained">
-										SignUp
-									</Button>
-								</Grid>
-							</form>
-							<Grid container>
-								<Grid item xs={8}>
-									{this.state.html}
+							<Grid item xs={12}>
+								<form onSubmit={this.doSubmit}>
+									<Grid
+										container
+										item
+										xs={12}
+										alignContent="center"
+										justify="center"
+									>
+										<TextField
+											type="email"
+											placeholder="メールアドレス"
+											variant="outlined"
+											id="email"
+											required
+										></TextField>
+									</Grid>
+									<Grid
+										container
+										item
+										xs={12}
+										alignContent="center"
+										justify="center"
+									>
+										<TextField
+											type="password"
+											placeholder="パスワード"
+											variant="outlined"
+											id="password"
+											required
+											minLength="8"
+										></TextField>
+									</Grid>
+									<Grid
+										container
+										item
+										xs={12}
+										alignContent="center"
+										justify="center"
+									>
+										<TextField
+											type="password"
+											placeholder="パスワード(確認用)"
+											variant="outlined"
+											id="password-check"
+											required
+											minLength="8"
+										></TextField>
+									</Grid>
+									<Grid
+										container
+										item
+										xs={12}
+										alignContent="center"
+										justify="center"
+									>
+										<Button type="submit" variant="contained">
+											SignUp
+										</Button>
+									</Grid>
+								</form>
+								<Grid container>
+									<Grid item xs={12}>
+										{this.state.html}
+									</Grid>
 								</Grid>
 							</Grid>
 						</Paper>
