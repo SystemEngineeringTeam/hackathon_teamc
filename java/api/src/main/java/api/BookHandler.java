971b16b45b8178cf8544ddd33e5bd7fc7caa10cf
yoshikawataiki <yoshikawataiki@gmail.com>
2021-03-17 23:11:17 +0900

    [修正]gradleでコンパイルできるようにしました

--- java/api/src/main/java/api/BookHandler.java
+++ java/api/src/main/java/api/BookHandler.java
@@ -1,4 +1,3 @@
-
 package api;
 
 import database.booksql.*;
@@ -58,7 +57,9 @@ public class BookHandler implements HttpHandler {
 
             ObjectMapper mapper = new ObjectMapper();
             resBody = mapper.writeValueAsString(Get);
-            //resBody = "[{\"id\": 0,\"title\": \"string\",\"author\": \"string\",\"publisher\": \"string\",\"publishYear\": \"string\",\"cover\": \"string\",\"tags\": [\"string\"]}]";
+            // resBody = "[{\"id\": 0,\"title\": \"string\",\"author\":
+            // \"string\",\"publisher\": \"string\",\"publishYear\": \"string\",\"cover\":
+            // \"string\",\"tags\": [\"string\"]}]";
             System.out.println(resBody);
             break;
 
